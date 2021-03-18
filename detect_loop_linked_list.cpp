3107e0371b9840c45fdb63d52816effd9a5eac6a
Ayush Chaudhary <ayush2918@gmail.com>
2021-03-18 16:34:24 +0530

    +1

---
+++ detect_loop_linked_list.cpp
@@ -0,0 +1,103 @@
+// { Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+ // } Driver Code Ends
+/*
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+    bool detectLoop(Node* head)
+    {
+        Node *h=head,*t=head;
+        if(!h)
+        return false;
+        while(h->next!=0&&t!=0)
+        {
+            h=h->next->next;
+            t=t->next;
+            if(!t||!h)
+            {
+                return 0;
+            }
+            if(h==t)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        if(ob.detectLoop(head) )
+            cout<< "True\n";
+        else
+            cout<< "False\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends
