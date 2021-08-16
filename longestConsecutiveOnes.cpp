8eeea93a01fce357d06c5e072656869c9150be8a
Ayush <ayush2918@gmail.com>
2021-08-16 14:36:48 +0530

    16-08-21

---
+++ longestConsecutiveOnes.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*  Function to calculate the longest consecutive ones
+*   N: given input to calculate the longest consecutive ones
+*/
+class Solution
+{
+    public:
+    int maxConsecutiveOnes(int N)
+    {
+        int i=1;int count=0,maxCount=0;
+        while(i<=N)
+        {
+            if(i&N)
+            {
+                count++;
+            }
+            else
+            {
+                count=0;
+            }
+            maxCount=max(maxCount,count);
+            i<<=1;
+        }
+        return maxCount;
+    }
+};
+
+
+// { Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		Solution obj;
+		//calling maxConsecutiveOnes() function
+		cout<<obj.maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
